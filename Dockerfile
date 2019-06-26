b93536781bae39ec9acf2186bb7b02c51f0cc8db
Marcel Plch <mplch@redhat.com>
2019-06-26 16:29:23 +0200

    Make the reproducer less prone to breakage

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM fedora:rawhide
 ADD repro.py /repro.py
 ADD reproduce.sh /reproduce.sh
 ADD config.sh /config.sh
+ADD local.repo /etc/yum.repos.d/local.repo
 RUN chmod +x config.sh reproduce.sh
 RUN ./config.sh
--- reproduce.sh
+++ reproduce.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-cp repro.py /scipy/scipy-1.2.1/build/lib.linux-x86_64-3.8/
-cd /scipy/scipy-1.2.1/build/lib.linux-x86_64-3.8/
+cp repro.py /scipy/scipy-1.2.1/build/lib.linux-x86_64-3.7/
+cd /scipy/scipy-1.2.1/build/lib.linux-x86_64-3.7/
 python3 repro.py
